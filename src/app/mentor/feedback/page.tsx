6917f23e10edce44f8a6090e1d0ede9b7f022e2c
piousdev <piousalpha@gmail.com>
2025-07-06 04:30:44 +0200

    refactor: update FeedbackPage component for improved user guidance
    - Enhance the description in MentorHeader to clarify the purpose of the feedback form.
    - Maintain existing layout and styling while ensuring a consistent user experience.

--- src/app/mentor/feedback/page.tsx
+++ src/app/mentor/feedback/page.tsx
@@ -3,16 +3,16 @@ import { ContactForm } from "@/modules/mentor/components/feedback-contact-form";
 import MentorHeader from "@/modules/mentor/components/mentor-header";
 
 export default function FeedbackPage() {
-  return (
-    <div className="bg-background text-foreground flex items-center justify-center p-6">
-      <div className="flex flex-col gap-12 w-full max-w-3xl">
-        <MentorHeader
-          title="Get in touch"
-          description="Fill out the form below to provide us with your feedback."
-        />
-        <ContactForm />
-      </div>
-      <Toaster />
-    </div>
-  );
+	return (
+		<div className="bg-background text-foreground flex items-center justify-center p-6">
+			<div className="flex flex-col gap-12 w-full max-w-3xl">
+				<MentorHeader
+					title="Get in touch"
+					description="Fill out the form below to provide us with your feedback, questions or concerns."
+				/>
+				<ContactForm />
+			</div>
+			<Toaster />
+		</div>
+	);
 }
